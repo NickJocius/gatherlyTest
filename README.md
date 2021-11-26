164f5823563b326f274344d54321ed61f60135ee
Nick Jocius <capacity512@yahoo.com>
2021-11-26 16:27:55 -0600

    added api address for local enviornment

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # gatherlyTest
+
+## Third Party Libraries
+
+nanoid
+mongoose
+axios
+dotenv
+material-ui
+
+## Run App
+
+From root, use command npm run dev to start both backend and front end.
+
+## Database Info
+
+.env file included in email for mongodb connection and enrviornment variables
--- client/src/services/urlFunctions.js
+++ client/src/services/urlFunctions.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
+
 export const shortenUrl = async (origUrl) => {
-    return axios.post(`${process.env.REACT_APP_API}/url`, {origUrl: origUrl});
+    return axios.post(`http://localhost:5000/api/url`, {origUrl: origUrl});
 }
 
 export const getUrl = async (shortUrl) => {
     let modUrl = shortUrl.replace('https://', '');
-    return axios.get(`${process.env.REACT_APP_API}/url/${modUrl}`);
+    return axios.get(`http://localhost:5000/api/url/${modUrl}`);
 }
